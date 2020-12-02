4dc63a224755b45fe613b5431d43209cada9f2a7
Andreas Lymalm <a.lymalm@gmail.com>
2020-12-02 06:21:19 +0100

    Setup for day 2

---
+++ src/day_2/solution.rs
@@ -0,0 +1,21 @@
+use crate::common;
+
+use std::io::{Error, ErrorKind};
+
+pub fn part_1() -> Result<i32, Error>  {
+    let lines = common::read_input_for_day(1);
+    for i in 0..lines.len() {
+        
+    }
+    
+    return Err(Error::new(ErrorKind::Other, "Solution not found"));
+}
+
+pub fn part_2() -> Result<i32, Error>  {
+    let lines = common::read_input_for_day(1);
+    for i in 0..lines.len() {
+        
+    }
+    
+    return Err(Error::new(ErrorKind::Other, "Solution not found"));
+}
