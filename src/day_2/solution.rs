3df82e7ead8234a36c35d9623435d0a024c0024a
Andreas Lymalm <a.lymalm@gmail.com>
2020-12-02 06:45:51 +0100

    Solution - part 1 (day 2)

--- src/day_2/solution.rs
+++ src/day_2/solution.rs
@@ -3,16 +3,35 @@ use crate::common;
 use std::io::{Error, ErrorKind};
 
 pub fn part_1() -> Result<i32, Error>  {
-    let lines = common::read_input_for_day(1);
+    let lines = common::read_input_for_day(2);
+
+    let mut valid_passwords = 0;
     for i in 0..lines.len() {
-        
+        // Parse line
+        let line: Vec<&str> = lines[i].split(": ").collect();
+        let policy: Vec<&str> = line[0].split(" ").collect();
+        let lengths: Vec<&str> = policy[0].split("-").collect();
+        let min_length = lengths[0].parse::<i32>().unwrap();
+        let max_length = lengths[1].parse::<i32>().unwrap();
+        let letter = policy[1].parse::<char>().unwrap();
+        let password = line[1];
+
+        let mut letter_count = 0;
+        for p in password.chars() {
+            if p == letter {
+                letter_count += 1;
+            }
+        }
+        if min_length <= letter_count && letter_count <= max_length {
+            valid_passwords += 1;
+        }
     }
     
-    return Err(Error::new(ErrorKind::Other, "Solution not found"));
+    return Ok(valid_passwords);
 }
 
 pub fn part_2() -> Result<i32, Error>  {
-    let lines = common::read_input_for_day(1);
+    let lines = common::read_input_for_day(2);
     for i in 0..lines.len() {
         
     }
