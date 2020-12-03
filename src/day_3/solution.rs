355120ca7dc49b9ee0f5a0df3394bc33e3b11dcc
Andreas Lymalm <a.lymalm@gmail.com>
2020-12-03 20:07:20 +0100

    Solution - part 1 (day 3)

--- src/day_3/solution.rs
+++ src/day_3/solution.rs
@@ -3,16 +3,27 @@ use crate::common;
 use std::io::{Error, ErrorKind};
 
 pub fn part_1() -> Result<i32, Error>  {
-    let lines = common::read_input_as_integers_for_day(3);
+    let lines = common::read_input_for_day(3);
+    
+    let mut tree_count = 0;
+    let line_length = lines[0].len();
+    let mut pos = 0;
     for i in 0..lines.len() {
-        
+        let line = &lines[i];
+        let terrain = line.chars().nth(pos).unwrap();
+
+        if terrain == '#' {
+            tree_count += 1;
+        }
+        pos += 3;
+        pos %= line_length;
     }
     
-    return Err(Error::new(ErrorKind::Other, "Solution not found"));
+    return Ok(tree_count);
 }
 
 pub fn part_2() -> Result<i32, Error>  {
-    let lines = common::read_input_as_integers_for_day(3);
+    let lines = common::read_input_for_day(3);
     
     
     return Err(Error::new(ErrorKind::Other, "Solution not found"));
